5dcd047bab3b47234a5d392a8f44e14004b91037
piotrkaminski95 <pkaminki95@gmail.com>
2018-09-10 21:01:35 +0200

    add TargetView

--- view/TargetView.js
+++ view/TargetView.js
@@ -7,10 +7,21 @@ export class TargetView {
     }
     
     render(target) {
-        
+        return `<div class="target">
+                    <img src="${target.targetImg}" alt="grave" id="graveImg">
+                    <div class="lifeBar" style="border: red 2px solid">
+                        <div class="lifeValue" value="${target.hp}" style="height: 24px; width: 100%; background-color: red"></div>
+                    </div>
+                </div>`;
     }
     
     createElement(target) {
-        
+        let elem = document.createElement("template");
+        elem.innerHTML = this.render(target);
+        return elem.content.firstChild;
+    }
+    
+    registerEventListeners() {
+        this.element.addEventListener('click', this.controller.handleClick.bind(this.controller));
     }
 }
